047c477d7fe83314a9c3ddf33c22c46e325801df
semvak164 <jefjefri170@gmail.com>
2025-09-15 13:27:28 +0700

    variabel

---
+++ variabel/latihan1.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>variabel</title>
+</head>
+<body>
+   <?php
+    $nama="nama";
+    echo $nama;
+    ?>
+</body>
+</html>
---
+++ variabel/latihan2.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>variabel latihan2</title>
+</head>
+<body>
+    <?php 
+       $nama="jefri nichol";
+       $kelas="XI RPL";
+       $alamat="jl. dauwan kaler,kp. sukamulya,RT 12 RW 04";
+
+
+       echo "nama : $nama";
+       echo "<br>";
+       echo "kelas: $kelas";
+       echo "<br>";
+       echo "alamat: $alamat";
+       ?>
+</body>
+</html>
